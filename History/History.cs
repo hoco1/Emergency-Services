c26d02e137ed67464d697362164e07d9b1a4439d
hoco1 <mohammadrezanorouzi600@gmail.com>
2023-04-07 12:56:41 +0430

    Create fields user,history

---
+++ History/History.cs
@@ -0,0 +1,10 @@
+namespace EmergencyServices
+{
+    public class History
+    {
+        public User user;
+        public DateTime Request { get; set; }
+        public string ServiceType { get; set; }
+
+    }
+}
---
+++ User/User.cs
@@ -0,0 +1,11 @@
+namespace EmergencyServices
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Gender { get; set; }
+        public string IdentityNumber { get; set; }
+        public string Address { get; set; }
+
+    }
+}
